a5f98b85e65e91ea8f60fb2bb61072ab404d0d90
Seline <selinehaniff23@gmail.com>
2025-01-24 05:23:18 +0000

    finished

--- test/test.js
+++ test/test.js
@@ -11,3 +11,55 @@ import {
   exponentiation,
 } from "../src/index.js";
 
+describe ("helloWorld Function", () => {
+  it("should return a string", () => {
+    const result = helloWorld();
+    assert.typeOf(result, "string", "the return value should be a string")
+  });
+  it ('should return "Hello World"', () => {
+    const result = helloWorld();
+    assert.equal(result, "Hello World", "The result should be 'Hello World'");
+  });
+});
+
+describe ("addition", () => {
+  it("should return the sum of two numbers", () => {
+    const result = addition(5, 10);
+    assert.equal(result, 15, "5 + 10 should equal 15");
+  });
+});
+
+describe ("subtraction", () => {
+  it("should return the subtraction of two numbers", () => {
+    const result = subtraction(10, 5);
+    assert.equal(result, 5, "10 - 5 should equal 5");
+  });
+});
+
+describe ("multiplication", () => {
+  it("should return the multiplication of two numbers", () => {
+    const result = multiplication(5, 5);
+    assert.equal(result, 25, "5 * 5 should equal 25");
+  });
+});
+
+describe ("division", () => {
+  it("should return the division of two numbers", () => {
+    const result = division(10, 5);
+    assert.equal(result, 2, "10 / 5 should equal 2");
+  });
+});
+
+describe ("remainder", () => {
+  it("should return the remainder of two numbers", () => {
+    const result = remainder(10, 5);
+    assert.equal(result, 0, "10 % 5 should equal 0");
+  });
+});
+
+describe ("exponentiation", () => {
+  it("should return the exponent of two numbers", () => {
+    const result = exponentiation(2, 2);
+    assert.equal(result, 4, "2 to the power of 2 should equal 4");
+  });
+});
